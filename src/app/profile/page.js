50d1ebf8c5c208685ccae068a0adb6a7224b1c6e
vzla0094 <vzla0094@gmail.com>
2024-03-27 17:36:29 -0600

    add Profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,16 @@
-import {Container} from "@mui/material";
+import {Avatar, Box} from "@mui/material";
+import {Person} from "@mui/icons-material";
 
 export default function Profile() {
   return (
-    <Container>
-      <p>Blank Profile page</p>
-    </Container>
+    <Box p={10} display='flex' flexDirection='column' alignItems='center'>
+      <Avatar sx={{width: 150, height: 150, mb: 5}}>
+        <Person/>
+      </Avatar>
+      <p><b>Name: </b>Eliezer</p>
+      <p><b>Age: </b>29 years</p>
+      <p><b>Hobbies: </b>Hiking</p>
+      <p><b>Books: </b>Man in search for meaning - Viktor Frankl</p>
+    </Box>
   )
 }
