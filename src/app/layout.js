7a8739331f1b1bd3d86ad5be5132a0ebb5e01a32
vzla0094 <vzla0094@gmail.com>
2024-03-27 17:15:15 -0600

    update Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,18 @@
 import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter'
 import {Inter} from "next/font/google";
 import {BottomNav} from "@/components/BottomNav";
+import {Container} from "@mui/material";
 
 const inter = Inter({subsets: ["latin"]});
 
 export default function RootLayout({children}) {
   return (
-    <html lang="en">
-    <body className={inter.className}>
+    <html lang="en" style={{height: '100%'}}>
+    <body className={inter.className} style={{minHeight: '100%', display: 'flex', flexDirection: 'column'}}>
     <AppRouterCacheProvider>
-      {children}
+      <Container sx={{flex: 1}}>
+        {children}
+      </Container>
       <BottomNav/>
     </AppRouterCacheProvider>
     </body>
--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import {Post} from "@/components/Post";
 
 export default function Home() {
   return (
-    <Container>
+    <>
       <Stack spacing={1}>
         <Post author='Valentina'
               text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper magna eu libero placerat, vitae fermentum libero malesuada. Phasellus consequat ante sed enim ultricies, eu aliquet mi pulvinar. Nulla facilisi. Mauris vitae consequat velit.'/>
         <Post author='Carlos'
               text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper magna eu libero placerat, vitae fermentum libero malesuada. Phasellus consequat ante sed enim ultricies, eu aliquet mi pulvinar. Nulla facilisi. Mauris vitae consequat velit.'/>
       </Stack>
-    </Container>
+    </>
   );
 }
