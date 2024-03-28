0f69110ee5112b879ad00762a6cc03ca8295303b
vzla0094 <vzla0094@gmail.com>
2024-03-27 18:07:48 -0600

    add Post detailed view

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,40 @@
-import {Avatar, Box, Card} from "@mui/material";
+'use client'
+
+import {useState} from "react";
+import {Avatar, Box, Card, CardActionArea, Dialog, DialogContent, Typography} from "@mui/material";
 import {Person} from "@mui/icons-material";
 
-export const Post = ({author, text, profilePicture}) => <Card sx={{p: 2}}>
+const PostContent = ({detailedView = false, text, author}) => <>
   <Box display="flex" alignItems="center">
     <Avatar sx={{mr: 2}}>
       <Person/>
     </Avatar>
     <p>{author}</p>
   </Box>
-  <p>{text}</p>
-</Card>;
+  <Typography noWrap={!detailedView}>{text}</Typography>
+</>
+
+export const Post = ({author, text}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return <>
+    <Dialog fullWidth maxWidth='xl' onClose={handleClose} open={open}>
+      <DialogContent>
+        <PostContent detailedView author={author} text={text}/>
+      </DialogContent>
+    </Dialog>
+    <Card>
+      <CardActionArea sx={{p: 2}} onClick={handleClickOpen}>
+        <PostContent author={author} text={text}/>
+      </CardActionArea>
+    </Card>
+  </>;
+}
