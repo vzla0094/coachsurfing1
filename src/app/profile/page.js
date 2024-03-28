86f03cb865f2284e99cef79bf4e662839b3a6222
vzla0094 <vzla0094@gmail.com>
2024-03-27 19:02:58 -0600

    add users mock data and wire up to profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,16 +1,30 @@
+'use client'
+
+import {useEffect, useState} from "react";
+
 import {Avatar, Box} from "@mui/material";
 import {Person} from "@mui/icons-material";
 
+import data from '/src/users.json'
+
 export default function Profile() {
+  const [user, setUser] = useState({})
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setUser(data['axtykgvdtyj924'])
+    }, 500)
+  }, [])
+  
   return (
     <Box p={10} display='flex' flexDirection='column' alignItems='center'>
       <Avatar sx={{width: 150, height: 150, mb: 5}}>
         <Person/>
       </Avatar>
-      <p><b>Name: </b>Eliezer</p>
-      <p><b>Age: </b>29 years</p>
-      <p><b>Hobbies: </b>Hiking</p>
-      <p><b>Books: </b>Man in search for meaning - Viktor Frankl</p>
+      <p><b>Name: </b>{user.name}</p>
+      <p><b>Age: </b>{user.age}</p>
+      <p><b>Hobbies: </b>{user.hobbies?.map(hobby => `${hobby} `)}</p>
+      <p><b>Books: </b>{user.books?.map(book => `${book} `)}</p>
     </Box>
   )
 }
