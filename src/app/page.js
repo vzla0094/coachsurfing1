3aa2b64ba56c2cb1da1f4016ffb277cd1cfac095
vzla0094 <vzla0094@gmail.com>
2024-03-27 18:33:57 -0600

    add postId and userId reference to Post object

--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,25 @@ import data from '/src/posts.json'
 
 export default function Home() {
   const [posts, setPosts] = useState([])
-
+  
   useEffect(() => {
     // TODO: improve mock call with Promises
     setTimeout(() => {
       setPosts(data.posts)
     }, 2000)
   }, [])
-
+  
   return (
     <>
       <Stack spacing={1}>
-        {posts.map(({author, text, picture}) => <Post author={author} text={text} picture={picture}/>)}
+        {posts.map(({author, text, picture, postId}) =>
+          <Post
+            key={postId}
+            author={author}
+            text={text}
+            picture={picture}
+          />
+        )}
       </Stack>
     </>
   );
